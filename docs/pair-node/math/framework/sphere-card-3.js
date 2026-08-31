3ae3545b7849b5d231ec2e5c001f790b42ba1c68
David Tauraso <david318@me.com>
2026-08-31 00:09:49 -0700

    The acute top vector carries the 1 like the distance on the three-node card

--- docs/pair-node/math/framework/sphere-card-3.js
+++ docs/pair-node/math/framework/sphere-card-3.js
@@ -47,7 +47,7 @@ const SPHERE_CARD_3_FORMULAS = String.raw`\[
 \begin{bmatrix} \text{distance}^{2}_{\text{bottom}_{\varphi}} \\ \text{distance}^{2}_{\text{bottom}_{\theta}} \end{bmatrix}
   &=& \begin{bmatrix} |\, \text{bottom}^{k}_{\varphi} - \text{arrival}^{2}_{\varphi} \,| \\[6pt]
                       |\, \text{bottom}^{k}_{\theta} - \text{arrival}^{2}_{\theta} \,| \end{bmatrix} \\[10pt]
-\begin{bmatrix} \text{acute}_{\text{top}_{\varphi}} \\ \text{acute}_{\text{top}_{\theta}} \end{bmatrix}
+\begin{bmatrix} \text{acute}^{1}_{\text{top}_{\varphi}} \\ \text{acute}^{1}_{\text{top}_{\theta}} \end{bmatrix}
   &=& \begin{bmatrix} \text{distance}^{1}_{\text{top}_{\varphi}} & \text{if } \text{distance}^{1}_{\text{top}_{\varphi}} < \tau_{\varphi}/4 \\[3pt]
                       0 & \text{otherwise} \\[6pt]
                       \text{distance}^{1}_{\text{top}_{\theta}} & \text{if } \text{distance}^{1}_{\text{top}_{\theta}} < \tau_{\theta}/4 \\[3pt]
