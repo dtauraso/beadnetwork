c7f0652306e07eb173b21cf5d0ea5feba720ed61
David Tauraso <david318@me.com>
2026-09-18 01:34:52 -0700

    Revert "Delta 1 is added to the six"
    This reverts commit aeb4f3bdfb00f4f05f6833b94b1fe972896e5362.

--- docs/pair-node/math/framework/sphere-card-3.js
+++ docs/pair-node/math/framework/sphere-card-3.js
@@ -74,12 +74,12 @@ b &=& \text{arrival 2's quadrant} \\[3pt]
                       0 & \text{otherwise} \end{bmatrix} \\[10pt]
 \Delta_{1\,\varphi},\, \Delta_{1\,\theta} &\in& \{3, 4, 5, 6\} \\[6pt]
 \begin{bmatrix} p_{1}r_{1\,\varphi} \\ p_{1}r_{1\,\theta} \end{bmatrix}
-  &=& \begin{bmatrix} (6 + \Delta_{1\,\varphi}) - \text{arrival}_{\varphi} \\[6pt]
-                      (6 + \Delta_{1\,\theta}) - \text{arrival}_{\theta} \end{bmatrix} \\[6pt]
+  &=& \begin{bmatrix} (6 - \Delta_{1\,\varphi}) - \text{arrival}_{\varphi} \\[6pt]
+                      (6 - \Delta_{1\,\theta}) - \text{arrival}_{\theta} \end{bmatrix} \\[6pt]
 \begin{bmatrix} x_{1\,\varphi} \\ x_{1\,\theta} \end{bmatrix}
-  &=& \begin{bmatrix} -1 & \text{if } 6 + \Delta_{1\,\varphi} < p_{1}r_{1\,\varphi} < 3 \\[3pt]
+  &=& \begin{bmatrix} -1 & \text{if } 6 - \Delta_{1\,\varphi} < p_{1}r_{1\,\varphi} < 3 \\[3pt]
                       0 & \text{otherwise} \\[6pt]
-                      -1 & \text{if } 6 + \Delta_{1\,\theta} < p_{1}r_{1\,\theta} < 3 \\[3pt]
+                      -1 & \text{if } 6 - \Delta_{1\,\theta} < p_{1}r_{1\,\theta} < 3 \\[3pt]
                       0 & \text{otherwise} \end{bmatrix} \\[10pt]
 \Delta_{2\,\varphi},\, \Delta_{2\,\theta} &\in& \{6, 7, 8, 9\} \\[6pt]
 \begin{bmatrix} p_{2}r_{2\,\varphi} \\ p_{2}r_{2\,\theta} \end{bmatrix}
